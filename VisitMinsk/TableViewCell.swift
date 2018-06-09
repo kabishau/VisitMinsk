829c5847b63b6d5d2ebfb6ac6bab3c102a5b372e
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-06-08 23:40:44 -0400

    Add .gitignore file

---
+++ VisitMinsk/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  VisitMinsk
+//
+//  Created by Aleksey Kabishau on 0608..18.
+//  Copyright © 2018 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
