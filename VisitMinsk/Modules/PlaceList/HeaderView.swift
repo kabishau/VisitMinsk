e8ea6c17b5d03c5b783583a894dd5a3a835a29f9
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-06-26 00:01:46 -0400

    header view of the table view cell

---
+++ VisitMinsk/Modules/PlaceList/HeaderView.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class HeaderView: UITableViewCell {
+
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    @IBOutlet weak var seeAllButton: UIButton!
+    
+    @IBAction func seeAllButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- VisitMinsk/Modules/PlaceList/PlaceList.swift
+++ VisitMinsk/Modules/PlaceList/PlaceList.swift
@@ -1,15 +1,20 @@
 import UIKit
 
+var headerHeight: CGFloat = 24.0
+
 class PlaceListViewController: UIViewController {
     
    var images = ["1_Church_Of_Sains_Simon_and_Helena_visit_in", "2_Church_Of_Sains_Simon_and_Helena_visit_in", "3_Church_Of_Sains_Simon_and_Helena_visit_in", "4_Church_Of_Sains_Simon_and_Helena_visit_in", "5_Church_Of_Sains_Simon_and_Helena_visit_in"]
     
-    var categories = ["Visit in the City", "Visit outside the City", "Food"]
+    var categories = ["VISIT IN THE CITY", "VISIT OUTSIDE THE CITY", "FOOD"]
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // initiation of stucture with local images
+        // arrayOfObjects = [objectStruct.init(properties of the struct)
 
     }
 
@@ -21,10 +26,21 @@ extension PlaceListViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return categories.count
     }
-
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "\(categories[section])"
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return headerHeight
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! HeaderView
+        headerView.categoryNameLabel.text = categories[section]
+        
+        return headerView
+    }
+
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "\(categories[section])"
+//    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
