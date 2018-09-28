19bdcbde787bdb68435164e41c09afa2d6252583
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-28 17:44:33 -0400

    setting the size of category cell using flow layout delegate and moving the class to separate file

---
+++ VisitMinsk/CategoryCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    // why init should be overriden?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        backgroundColor = UIColor.black
+    }
+}
--- VisitMinsk/MainScreenController.swift
+++ VisitMinsk/MainScreenController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 private let cellId = "cellId"
 
-class MainScreenController: UICollectionViewController {
+class MainScreenController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,20 +20,10 @@ class MainScreenController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
+    
+    // defines dimensions and spacing, requere delegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
 }
 
-class CategoryCell: UICollectionViewCell {
-    // why init should be overriden
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupView() {
-        backgroundColor = UIColor.red
-    }
-}
