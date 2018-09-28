70085af82abf371bcad2f2f34ff9e8167404a54b
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-09-28 00:22:32 -0400

    update

---
+++ VisitMinsk/MainScreenController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MainScreenController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+}
+
+class CategoryCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        
+    }
+}
